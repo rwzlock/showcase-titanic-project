cb234d87b56853630954d95fcc80d20e5e9ef0a7
Sinan Cengiz <sinancengiz_84@hotmail.com>
2018-08-11 20:54:58 -0700

    Charts  section added to html and passenger by gender chart updated

--- static/js/index.js
+++ static/js/index.js
@@ -33,9 +33,9 @@ Plotly.d3.json(plots_url, function(error, response) {
     var data = [response];
     console.log(response)
     var layout = { margin: { t: 30, b:50 },
-                   title: "Number of New User by Gender",
-                   xaxis: { title: "User by Gender "},
-                   yaxis: { title: "Number of New User"}            
+                   title: "Number of Passengers by Gender",
+                   xaxis: { title: "Passenger by Gender "},
+                   yaxis: { title: "Number of Passenger"}            
     }
     Plotly.plot("bar3", data, layout)
 })
